7ab7e4ffbaa01bc3c337682a805d265ad3fe9143
cherchok <viktor.yurzanov@gmail.com>
2018-07-21 10:59:47 +0300

    last update

--- src/main/java/edu/rest/MyLibrary.java
+++ src/main/java/edu/rest/MyLibrary.java
@@ -1,24 +1,46 @@
 package edu.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/myapp")
 public class MyLibrary {
 
-    @Autowired
-    B b;
+//    @Autowired
+    //  B b;
 
-    @GetMapping("sum")
+    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public int doAdd(@RequestParam("a") int a, @RequestParam("b") int b) {
-        return  a + b;
+    public MyVal doAdd(//@RequestParam("a") int a, @RequestParam("b") int b,
+                                        @RequestParam Map<String, String> params) {
+        int sum = 0;
+        for (Map.Entry<String, String> stringStringEntry : params.entrySet()) {
+            System.out.printf("%s=%s\n", stringStringEntry.getKey(), stringStringEntry.getValue());
+            try {
+                int i = Integer.parseInt(stringStringEntry.getKey());
+                sum += i;
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return new MyVal(sum, params.size());
+
+//        return new ResponseEntity<>(new MyVal(sum), HttpStatus.OK);
     }
 
+
+    //
+//  MyVal  sum(@RequestBody MyParam p)
+
     @ExceptionHandler
-    public void handler(Throwable e){
+    public void handler(Throwable e) {
         System.out.println(e);
     }
 }
---
+++ src/main/java/edu/rest/MyVal.java
@@ -0,0 +1,27 @@
+package edu.rest;
+
+public class MyVal {
+    private int v;
+    private int param2;
+
+    public MyVal(int v, int param2) {
+        this.v = v;
+        this.param2 = param2;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    public int getParam2() {
+        return param2;
+    }
+
+    public void setParam2(int param2) {
+        this.param2 = param2;
+    }
+}
